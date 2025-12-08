25dcf980a6de847068b5035a5b661de4073bfbb6
ShantheriNayak <shantherinayak@gmail.com>
2025-12-08 12:06:31 +0530

    defined func

--- learning_GIT.py
+++ learning_GIT.py
@@ -1,6 +1,9 @@
-num=10
-if num%2==0:
-    print(f"{num} is even")
-else:
-    print(f"{num} is odd")
-    
+def odd_even(start ,end):
+
+    for num in range(start, end):
+        if num%2==0:
+            print(f"{num} is even")
+        else:
+            print(f"{num} is odd")
+odd_even(100,200)
+
