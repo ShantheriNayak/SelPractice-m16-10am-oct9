ad0b29cae8f962a42780a11d3cf0c4995c2fe15c
ShantheriNayak <shantherinayak@gmail.com>
2025-12-09 11:00:10 +0530

    user input

--- learning_GIT.py
+++ learning_GIT.py
@@ -1,9 +1,6 @@
-def odd_even(start ,end):
-
-    for num in range(start, end):
-        if num%2==0:
-            print(f"{num} is even")
-        else:
-            print(f"{num} is odd")
-odd_even(100,200)
+num=int(input('enter num:'))
+if num%2==0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
 
